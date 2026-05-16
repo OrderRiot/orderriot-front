864ea2214c718807f096715d1370a7c17bb451a5
omwani <omwani03@gmail.com>
2026-05-16 21:02:40 +0530

    changed cta on landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -69,7 +69,7 @@ export default function Landing() {
               </Link>
             </Button>
             <Button asChild variant="outline" size="lg">
-              <Link to="/create">Start a campaign</Link>
+              <Link to="/ideas/new">Share an idea</Link>
             </Button>
           </div>
         </div>
